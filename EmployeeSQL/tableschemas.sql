d09cce6e7e4be5b1dcc73a3a042626de5b4917fc
karleecrose <crosekarlee@gmail.com>
2024-11-06 12:47:55 -0600

    Add to repository

---
+++ EmployeeSQL/tableschemas.sql
@@ -0,0 +1,67 @@
+-- Create departments table
+CREATE TABLE departments (
+	dept_no VARCHAR(50) primary key NOT NULL,
+	dept_name VARCHAR(50) NOT NULL
+);
+
+
+-- Create dept_manager table
+CREATE TABLE dept_manger (
+	dept_no VARCHAR(15) NOT NULL,
+	emp_no INT NOT NULL
+);
+
+-- Create employees table
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(10) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(5) NOT NULL,
+	hire_date DATE NOT NULL
+)
+
+--Create salaries table
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT NOT NULL
+);
+
+--Create titles table
+CREATE TABLE titles (
+	title_id VARCHAR(20) PRIMARY KEY NOT NULL,
+	title VARCHAR(50) NOT NULL
+);
+
+
+-- Create dept_emp table
+CREATE TABLE dept_emp (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR(4)   NOT NULL,
+    CONSTRAINT pk_dept_emp PRIMARY KEY (emp_no,dept_no)
+);
+
+--Create salaries table
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary MONEY NOT NULL,
+    CONSTRAINT pk_salaries PRIMARY KEY (emp_no)
+);
+
+-- Add contraints to tables created
+
+ALTER TABLE employees ADD CONSTRAINT fk_employees_emp_title_id FOREIGN KEY(emp_title_id)
+REFERENCES titles (title_id);
+
+ALTER TABLE employees ADD CONSTRAINT CHK_sex_validation CHECK (sex IN ('F', 'M'))
+;
+
+ALTER TABLE salaries ADD CONSTRAINT fk_salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES employees (emp_no);
+
+ALTER TABLE dept_emp ADD CONSTRAINT fk_dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
